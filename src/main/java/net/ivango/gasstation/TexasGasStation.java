daa604350039506df9dd197a2b7ae50318e8f7f0
Ivan Golubev <ivan.golubev.spb@gmail.com>
2016-08-29 00:16:10 +0300

    implementing the gas station
    1. implementing the gas station in a thread-safe manner
    2. writing tests

--- src/main/java/net/ivango/gasstation/TexasGasStation.java
+++ src/main/java/net/ivango/gasstation/TexasGasStation.java
@@ -11,46 +11,94 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-import java.util.Collection;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.DoubleAdder;
 
 public class TexasGasStation implements GasStation {
 
+    private List<ConcurrentGasPump> regularFilledPumps = new CopyOnWriteArrayList<>(),
+                                    superFilledPumps = new CopyOnWriteArrayList<>(),
+                                    dieselFilledPumps = new CopyOnWriteArrayList<>();
+
+    private List<GasPump> allGasPumps = new CopyOnWriteArrayList<>();
+    private Map<GasType, Double> price = new ConcurrentHashMap<>();
+
+    /* Accumulative statistics */
+    private DoubleAdder revenue = new DoubleAdder();
+    private AtomicInteger salesCount = new AtomicInteger(),
+                          cancellationsNoGasCount = new AtomicInteger(),
+                          cancellationsTooExpensiveCount = new AtomicInteger();
+
     private Logger logger = LoggerFactory.getLogger(TexasGasStation.class);
 
-
     public void addGasPump(GasPump gasPump) {
-
+        logger.info(String.format("Adding a gas pump: %s", gasPump.getGasType()));
+        getFilledPumps(gasPump.getGasType()).add( new ConcurrentGasPump(gasPump) );
+        allGasPumps.add(gasPump);
     }
 
-    public Collection<GasPump> getGasPumps() {
-        return null;
+    private List<ConcurrentGasPump> getFilledPumps(GasType gasType) {
+        switch(gasType) {
+            case REGULAR:   return regularFilledPumps;
+            case SUPER:     return superFilledPumps;
+            case DIESEL:    return dieselFilledPumps;
+        }
+        throw new IllegalArgumentException("Unknown gas type");
     }
 
-    public double buyGas(GasType gasType, double v, double v1) throws NotEnoughGasException, GasTooExpensiveException {
-        return 0;
+    public Collection<GasPump> getGasPumps() { return new ArrayList<>(allGasPumps); }
+
+    public double buyGas(GasType gasType, double amountInLiters, double maxPricePerLiter) throws NotEnoughGasException,
+                                                                                                 GasTooExpensiveException {
+        if (maxPricePerLiter < price.get(gasType)) {
+            cancellationsTooExpensiveCount.incrementAndGet();
+            logger.info(
+                    String.format("Requested price is too low: user requested to buy %s liters of %s for %s price.",
+                            amountInLiters, gasType, maxPricePerLiter)
+            );
+            throw new GasTooExpensiveException();
+        }
+
+        List<ConcurrentGasPump> filledPumps = getFilledPumps(gasType);
+        double earnings = 0;
+        if ( !filledPumps.isEmpty() ) {
+
+            List<ConcurrentGasPump> toRemove = new ArrayList<>();
+
+            for (ConcurrentGasPump gasPump: filledPumps) {
+
+                /* gas found and pumped - exit, we are selling at a maximum price */
+                if (gasPump.pumpedGas(amountInLiters)) {
+                    double moneyEarned = amountInLiters * maxPricePerLiter;
+                    revenue.add(moneyEarned);
+                    salesCount.incrementAndGet();
+                    logger.info(String.format("User bought %s liters of %s for %s", amountInLiters, gasType, moneyEarned));
+                    earnings = moneyEarned;
+                    break;
+                }
+                /* clean up the gas pump, so that we don't need to iterate over it the next time */
+                if (gasPump.isEmpty()) { toRemove.add(gasPump); }
+            }
+            filledPumps.removeAll(toRemove);
+        }
+        if ( earnings != 0.0d ) {
+            return earnings;
+        } else {
+            cancellationsNoGasCount.incrementAndGet();
+            logger.info(String.format("Cannot find %s liters of %s gas.", amountInLiters, gasType));
+            /* if we got here - no suitable pump was found */
+            throw new NotEnoughGasException();
+        }
     }
 
-    public double getRevenue() {
-        return 0;
-    }
+    public double getRevenue() { return revenue.sum(); }
+    public int getNumberOfSales() { return salesCount.get(); }
+    public int getNumberOfCancellationsNoGas() { return cancellationsNoGasCount.get(); }
+    public int getNumberOfCancellationsTooExpensive() { return cancellationsTooExpensiveCount.get(); }
 
-    public int getNumberOfSales() {
-        return 0;
-    }
-
-    public int getNumberOfCancellationsNoGas() {
-        return 0;
-    }
-
-    public int getNumberOfCancellationsTooExpensive() {
-        return 0;
-    }
-
-    public double getPrice(GasType gasType) {
-        return 0;
-    }
-
-    public void setPrice(GasType gasType, double v) {
-
-    }
+    public double getPrice(GasType gasType) { return price.get(gasType); }
+    public void setPrice(GasType gasType, double v) { price.put(gasType, v); }
 }
--- src/test/java/net/ivango/gasstation/TexasGasStationTest.java
+++ src/test/java/net/ivango/gasstation/TexasGasStationTest.java
@@ -1,13 +1,119 @@
 package net.ivango.gasstation;
 
+import net.bigpoint.assessment.gasstation.GasPump;
+import net.bigpoint.assessment.gasstation.GasType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.testng.Assert.assertEquals;
 
 public class TexasGasStationTest {
 
     private Logger logger = LoggerFactory.getLogger(TexasGasStationTest.class);
 
     @Test
-    public void test1(){}
+    public void setPriceTest() {
+        TexasGasStation gasStation = new TexasGasStation();
+        gasStation.setPrice(GasType.REGULAR, 1);
+        gasStation.setPrice(GasType.SUPER, 2);
+        gasStation.setPrice(GasType.DIESEL, 3);
+
+        assertEquals(gasStation.getPrice(GasType.REGULAR), 1.0d);
+        assertEquals(gasStation.getPrice(GasType.SUPER), 2.0d);
+        assertEquals(gasStation.getPrice(GasType.DIESEL), 3.0d);
+    }
+
+    @Test
+    public void addPumpsTest() {
+        TexasGasStation gasStation = new TexasGasStation();
+        gasStation.addGasPump( new GasPump(GasType.REGULAR, 3));
+        gasStation.addGasPump( new GasPump(GasType.SUPER, 3));
+        gasStation.addGasPump( new GasPump(GasType.DIESEL, 3));
+
+        assertEquals(gasStation.getGasPumps().size(), 3);
+    }
+
+    @Test
+    public void simplePurchaseTest() throws InterruptedException {
+        TexasGasStation gasStation = new TexasGasStation();
+        gasStation.setPrice(GasType.REGULAR, 1);
+        gasStation.setPrice(GasType.SUPER, 1);
+        gasStation.setPrice(GasType.DIESEL, 1);
+
+        gasStation.addGasPump( new GasPump(GasType.REGULAR, 3));
+        gasStation.addGasPump( new GasPump(GasType.SUPER, 3));
+        gasStation.addGasPump( new GasPump(GasType.DIESEL, 3));
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(4);
+        List<Callable<Void>> callables = new ArrayList<>();
+        for (int i=0; i < 3; i++) {
+            callables.add(() -> {
+                gasStation.buyGas(GasType.REGULAR, 1, 1);
+                return null;
+            });
+            callables.add(() -> {
+                gasStation.buyGas(GasType.SUPER, 1, 1);
+                return null;
+            });
+            callables.add(() -> {
+                gasStation.buyGas(GasType.DIESEL, 1, 1);
+                return null;
+            });
+        }
+        List<Future<Void>> futures = threadPool.invokeAll(callables);
+
+        assertEquals(gasStation.getNumberOfSales(), 9);
+        assertEquals(gasStation.getNumberOfCancellationsNoGas(), 0);
+        assertEquals(gasStation.getNumberOfCancellationsTooExpensive(), 0);
+        assertEquals(gasStation.getRevenue(), 9.0d);
+
+        threadPool.shutdownNow();
+    }
+
+    @Test
+    public void cancellationsTest() throws InterruptedException {
+        TexasGasStation gasStation = new TexasGasStation();
+        gasStation.setPrice(GasType.REGULAR, 9);
+        gasStation.setPrice(GasType.SUPER, 9);
+        gasStation.setPrice(GasType.DIESEL, 9);
+
+        gasStation.addGasPump( new GasPump(GasType.REGULAR, 3));
+        gasStation.addGasPump( new GasPump(GasType.SUPER, 3));
+        gasStation.addGasPump( new GasPump(GasType.DIESEL, 10));
+
+        ExecutorService threadPool = Executors.newFixedThreadPool(4);
+        List<Callable<Void>> callables = new ArrayList<>();
+
+        callables.add(() -> {
+            gasStation.buyGas(GasType.REGULAR, 1, 1);
+            return null;
+        });
+        callables.add(() -> {
+            gasStation.buyGas(GasType.SUPER, 1, 2);
+            return null;
+        });
+        for (int i=0; i<2; i++) {
+            callables.add(() -> {
+                gasStation.buyGas(GasType.DIESEL, 10, 10);
+                return null;
+            });
+        }
+
+        List<Future<Void>> futures = threadPool.invokeAll(callables);
+
+        assertEquals(gasStation.getNumberOfSales(), 1);
+        assertEquals(gasStation.getNumberOfCancellationsNoGas(), 1);
+        assertEquals(gasStation.getNumberOfCancellationsTooExpensive(), 2);
+        assertEquals(gasStation.getRevenue(), 100.0d);
+
+        threadPool.shutdownNow();
+    }
 }
